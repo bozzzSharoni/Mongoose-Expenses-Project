5acef2f605324592dbe0a5c85ab63e3aea4ad4c8
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-07-10 11:14:40 +0300

    added a server boilerplate

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express')
 const path = require('path')
+const request = require('request')
 
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'server')))
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+app.listen(port, function(){
+    console.log(`Running server on port ${port}`)
+})
